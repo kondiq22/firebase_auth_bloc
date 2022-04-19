0f53951e223a7ef35433117ffd60c92911ba05cf
kondiq22 <61063578+kondiq22@users.noreply.github.com>
2022-04-19 14:53:42 +0200

    Add AuthBloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_auth_bloc/pages/home_page.dart';
 import 'package:firebase_auth_bloc/pages/signin_page.dart';
 import 'package:firebase_auth_bloc/pages/signup_page.dart';
 import 'package:firebase_auth_bloc/pages/splash_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'blocs/auth/auth_bloc.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
+
+import 'repositories/auth_repository.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,18 +25,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Firebase Auth App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<AuthRepository>(
+          create: (context) => AuthRepository(
+              firebaseFirestore: FirebaseFirestore.instance,
+              firebaseAuth: FirebaseAuth.instance),
+        ),
+      ],
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider<AuthBloc>(
+            create: (context) =>
+                AuthBloc(authRepository: context.read<AuthRepository>()),
+          )
+        ],
+        child: MaterialApp(
+          title: 'Firebase Auth App',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: SplashPage(),
+          routes: {
+            SignupPage.routeName: (context) => SignupPage(),
+            SigninPage.routeName: (context) => SigninPage(),
+            HomePage.routeName: (context) => HomePage(),
+          },
+        ),
       ),
-      home: SplashPage(),
-      routes: {
-        SignupPage.routeName: (context) => SignupPage(),
-        SigninPage.routeName: (context) => SigninPage(),
-        HomePage.routeName: (context) => HomePage(),
-      },
     );
   }
 }
--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,15 +1,33 @@
+import 'package:firebase_auth_bloc/pages/signin_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../blocs/auth/auth_bloc.dart';
+import 'home_page.dart';
+
 class SplashPage extends StatelessWidget {
   static const String routeName = '/';
-  const SplashPage({ Key? key }) : super(key: key);
+  const SplashPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Home'),
-      ),
-      
+    return BlocConsumer<AuthBloc, AuthState>(
+      listener: (context, state) {
+        print('listener: $state');
+        if (state.authStatus == AuthStatus.unauthenticated) {
+          Navigator.pushNamed(context, SigninPage.routeName);
+        } else if (state.authStatus == AuthStatus.authenticated) {
+          Navigator.pushNamed(context, HomePage.routeName);
+        }
+      },
+      builder: (context, state) {
+        print('builder: $state');
+        return Scaffold(
+          body: Center(
+            child: Text('Home'),
+          ),
+        );
+      },
     );
   }
 }
