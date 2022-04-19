804b7ff5e6e5da27a3b2714b41bdb40bb8c67e01
kondiq22 <61063578+kondiq22@users.noreply.github.com>
2022-04-19 23:39:48 +0200

    HomePage Widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,10 @@
+import 'dart:ui';
+
+import 'package:firebase_auth_bloc/pages/profile_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../blocs/auth/auth_bloc.dart';
 
 class HomePage extends StatefulWidget {
   static const String routeName = '/home';
@@ -14,8 +20,44 @@ class _HomePageState extends State<HomePage> {
     return WillPopScope(
       onWillPop: () async => false,
       child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: Text('Home'),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.account_circle_outlined),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: ((context) {
+                    return ProfilePage();
+                  })),
+                );
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.exit_to_app),
+              onPressed: () {
+                context.read<AuthBloc>().add(SignoutRequestedEvent());
+              },
+            ),
+          ],
+        ),
         body: Center(
-          child: Text('Home'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/bloc_logo_full.png',
+                width: 350,
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Bloc is an awersome!\nThe best state management\nfor flutter!!!',
+                style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
         ),
       ),
     );
