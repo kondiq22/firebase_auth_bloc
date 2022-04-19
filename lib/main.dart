9277e267006cfd27703a6fd084ad851cf802331a
kondiq22 <61063578+kondiq22@users.noreply.github.com>
2022-04-19 13:13:03 +0200

    Pages structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'package:firebase_auth_bloc/pages/home_page.dart';
+import 'package:firebase_auth_bloc/pages/signin_page.dart';
+import 'package:firebase_auth_bloc/pages/signup_page.dart';
+import 'package:firebase_auth_bloc/pages/splash_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
@@ -16,58 +20,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Firebase Auth App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: SplashPage(),
+      routes: {
+        SignupPage.routeName: (context) => SignupPage(),
+        SigninPage.routeName: (context) => SigninPage(),
+        HomePage.routeName: (context) => HomePage(),
+      },
     );
   }
 }
---
+++ lib/pages/signup_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatefulWidget {
+  static const String routeName = '/signup';
+  const SignupPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Signup')),
+    );
+  }
+}
